767bc9b9798496126ab9f88a4b07f2052aa4c55f
ekondratovics <emils.kondratovics@gmail.com>
2021-04-24 13:06:32 +0300

    Update videoModal to use data- attributes as video url sources

--- resources/modules/videoModal.js
+++ resources/modules/videoModal.js
@@ -1,75 +1,58 @@
 let videoModal = () => {
-    let mediaQueryCarousel = window.matchMedia("(min-width: 1025px)");
+    let mediaQuery = window.matchMedia("(min-width: 1025px)");
     
     let playButtonsLeft = document.querySelectorAll(".watch-video-button.left"),
         playButtonsRight = document.querySelectorAll(".watch-video-button.right"),
         playButtonsMobile = document.querySelectorAll(".watch-video-button.mobile"),
-        hideWatch = document.querySelector(".watch-modal-close"),
-        watchModal = document.querySelector(".watch-modal"),
-        watchBody = document.querySelector("body"),
+        hideModal = document.querySelector(".watch-modal-close"),
+        modal = document.querySelector(".watch-modal"),
+        body = document.querySelector("body"),
         youTubeIframe = document.querySelector(".watch-video iframe");
 
-    let openWatchModalLeft = (videoIndex) => {
-        let youTubeLinksLeft = [
-            "https://www.youtube.com/embed/s3vjgaN2Tb0",
-            "https://www.youtube.com/embed/quBIbafdSRQ",
-            "https://www.youtube.com/embed/cMoeVdXv93k"
-        ];
-
-        youTubeIframe.setAttribute("src", youTubeLinksLeft[videoIndex]);
-        watchModal.classList.add("open");
-        watchBody.classList.add("open");
+    let addOpenToClassList = (target) => {
+        target.classList.add("open");
     }
 
-    let openWatchModalRight = (videoIndex) => {
-        let youTubeLinksRight = [
-            "https://www.youtube.com/embed/Usllgw9G2tU",
-            "https://www.youtube.com/embed/Epvj_qma2VM",
-            "https://www.youtube.com/embed/zN23rMWLirs"
-        ];
-
-        youTubeIframe.setAttribute("src", youTubeLinksRight[videoIndex]);
-        watchModal.classList.add("open");
-        watchBody.classList.add("open");
+    let openModalLeft = (videoIndex) => {
+        youTubeIframe.setAttribute("src", videoIndex);
+        addOpenToClassList(modal);
+        addOpenToClassList(body);
     }
 
-    let openWatchModalMobile = (videoIndex) => {
-        let youTubeLinksMobile = [
-            "https://www.youtube.com/embed/s3vjgaN2Tb0",
-            "https://www.youtube.com/embed/Usllgw9G2tU",
-            "https://www.youtube.com/embed/quBIbafdSRQ",
-            "https://www.youtube.com/embed/Epvj_qma2VM",
-            "https://www.youtube.com/embed/cMoeVdXv93k",
-            "https://www.youtube.com/embed/zN23rMWLirs"
-        ];
-
-        youTubeIframe.setAttribute("src", youTubeLinksMobile[videoIndex]);
-        watchModal.classList.add("open");
-        watchBody.classList.add("open");
+    let openModalRight = (videoIndex) => {
+        youTubeIframe.setAttribute("src", videoIndex);
+        addOpenToClassList(modal);
+        addOpenToClassList(body);
     }
 
-    let closeWatchModal = () => {
-        watchModal.classList.remove("open");
-        watchBody.classList.remove("open");
+    let openModalMobile = (videoIndex) => {
+        youTubeIframe.setAttribute("src", videoIndex);
+        addOpenToClassList(modal);
+        addOpenToClassList(body);
+    }
+
+    let closeModal = () => {
+        modal.classList.remove("open");
+        body.classList.remove("open");
         youTubeIframe.setAttribute("src", "");
     }
 
     let addEventListenersWatch = () => {
-        if(mediaQueryCarousel.matches) {
-            playButtonsLeft.forEach(function (button, index){
+        if(mediaQuery.matches) {
+            playButtonsLeft.forEach( (button) => {
                 button.addEventListener("click", function() {
-                    openWatchModalLeft(index);
+                    openModalLeft(this.dataset.url);
                 });
             });
-            playButtonsRight.forEach(function (button, index){
+            playButtonsRight.forEach( (button) => {
                 button.addEventListener("click", function() {
-                    openWatchModalRight(index);
+                    openModalRight(this.dataset.url);
                 });
             });
         } else {
-            playButtonsMobile.forEach(function (button, index) {
+            playButtonsMobile.forEach( (button) => {
                 button.addEventListener("click", function() {
-                    openWatchModalMobile(index);
+                    openModalMobile(this.dataset.url);
                 });
             });
         }
@@ -77,10 +60,10 @@ let videoModal = () => {
 
     addEventListenersWatch();
 
-    mediaQueryCarousel.addListener(addEventListenersWatch);
+    mediaQuery.addListener(addEventListenersWatch);
 
-    hideWatch.addEventListener("click", closeWatchModal);
-    watchModal.addEventListener("click", closeWatchModal);
+    hideModal.addEventListener("click", closeModal);
+    modal.addEventListener("click", closeModal);
 }
 
 export { videoModal };
